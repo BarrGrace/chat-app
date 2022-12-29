4228e7375b55ac486ed2475f81fe71be8e850789
BarrGrace <barr.grace@grunitech.com>
2022-12-30 01:43:43 +0200

    first commit

--- src/hooks/server-requests.ts
+++ src/hooks/server-requests.ts
@@ -1,53 +1,36 @@
 import { Message } from '../types/message';
-import { mockUsers } from '../assets/mockUsers'; // todo: remove this line after server implementation
+const endpoint = 'http://localhost:8000';
 
-const endpoint = '../assets/'; // todo: add endpoint (server) address (starting with http://)
-
-
-/**
- * GET Request to get the list of messages
- **/
 export async function getMessages() {
-  // todo: replace this with fetch to get the messages from the server
-  const { mockMessages } = await import(`${endpoint}/mockMessages`);
 
-  // todo: this should be implemented in the server. Chat Messages should already have the authors' names.
-  // todo: remove this mapping when getting the data from the server
-  const mockMessagesWithNames = mockMessages.map((message: Message) => {
-    const author = mockUsers.find(user => user.id === message.authorId);
-    const authorName = author && author.name;
-    return { ...message, authorName };
-  });
-
-  return mockMessagesWithNames;
+  return await fetch(endpoint + '/users')
+      .then((res) => res.json());
 }
-
-/**
- * GET request to get the full list of users - id + name
- **/
 export async function getUsers() {
-  // todo: replace this with fetch to get the user list from the server
-  const { mockUsers } = await import(`${endpoint}/mockUsers`);
-  return mockUsers;
+
+  return await fetch(`${endpoint}/mockUsers`)
+    .then((res) => res.json());
 }
 
-
-/**
- * GET request to get the full details of a user
- **/
 export async function getUserDetails(userId: number) {
-  // todo: replace this with fetch to get the user details from the server.
-  //  For mocking example, we're calling an external JSON service.
-  //  You can use mockUserDetails.ts for the list of user details in the server.
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users?id=${userId}`);
-  return (await res.json())[0];
+
+  const res = await fetch(`${endpoint}/users/${userId}`);
+  return (await res.json());
 }
 
-/**
- * POST request to add a message. The message contains: id, body, timestamp, authorId
- **/
 export async function addNewMessage(message: Message) {
-  // todo: implement sending a new message to the server
+  
+  const data = {
+
+    method: "Post",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(message)
+  }
+
+  const res = await fetch(endpoint + '/newMessages', data);
+  return await res.json();
 }
 
 /**
